18f3cec5cc963f5c4131e17f0b5fab5cc1909e8d
Meng Liu <veronica.lm@live.cn>
2022-02-20 22:00:33 +0000

    added new dataset for LDA

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,6 +3,8 @@
 usethis::use_data(DATASET, overwrite = TRUE)
 books <- rio::import("data-raw/books.rds")
 
+model <- rio::import("data-raw/model.rds")
+
 model_7 <- rio::import("data-raw/model_7.rds")
 
 new_books <- rio::import("data-raw/new_books.rds")
