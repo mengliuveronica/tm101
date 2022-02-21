99cf5ac2959b918f341f794b22e31884a095220d
Meng Liu <veronica.lm@live.cn>
2022-02-21 21:37:17 +0000

    added pos tagged dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
+
 books <- rio::import("data-raw/books.rds")
 
 model <- rio::import("data-raw/model.rds")
@@ -10,3 +11,5 @@ model_7 <- rio::import("data-raw/model_7.rds")
 new_books <- rio::import("data-raw/new_books.rds")
 
 new_dtm <- rio::import("data-raw/new_dtm.rds")
+
+by_chapter_pos <- rio::import("data-raw/by_chapter_pos.rds")
